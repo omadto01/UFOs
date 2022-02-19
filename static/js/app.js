85b97fdb2fa9cd45b482da34387d15c6eaec932f
omadto01 <omadto.omar@hotmail.com>
2022-02-19 17:05:10 -0500

    Challeng redo

--- static/js/app.js
+++ static/js/app.js
@@ -57,13 +57,14 @@ function updateFilters() {
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    Object.entries(filters).forEach(function([key,value]){
+    Object.entries(filters).forEach(([key,value]) => {
 
   
 
     // 10. Finally, rebuild the table using the filtered data
     filteredData =filteredData.filter((row) => row[key] === value);
-    })
+    });
+    buildTable(filteredData);
   }
   
   // 2. Attach an event to listen for changes to each filter
